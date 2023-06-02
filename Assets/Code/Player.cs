040075d5ca82689f6b7591f18f40fb72c88a1c7f
Tonza81 <129681977+Tonza81@users.noreply.github.com>
2023-06-02 14:03:49 +0300

    Ampumis - liikkumispäivitys
    Rajattu pelialue karkeasti näytön kokoiselle alueelle.
    
    +
    
    Kala osaa nyt:
    
    Juosta = shift
    Ampua = spacebar
    Ampua sarjatulta = left CTRL

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    float moveSpeed = 3;
+    Gun[] guns;
+
+    public float moveSpeed = 3;
 
     bool moveUp;
     bool moveDown;
     bool moveLeft;
     bool moveRight;
 
+    bool speedUp;
 
-    // Start is called before the first frame update
+    bool shoot;
+
+
+
+
     void Start()
-    {
-        
+    {   
+        // Kun Player luodaan sceneen etsitään kaikki aseet jotka ovat siinä kiinni (in children)
+        guns = transform.GetComponentsInChildren<Gun>();
     }
 
     // Update is called once per frame
@@ -26,6 +34,20 @@ public class Player : MonoBehaviour
         moveDown = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
         moveLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
         moveRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+
+        speedUp = Input.GetKey(KeyCode.LeftShift)||Input.GetKey(KeyCode.RightShift);
+
+
+        //Space ampuu kerran & LCTRL ampuu sarjaa
+        shoot = Input.GetKeyDown(KeyCode.Space)||Input.GetKey(KeyCode.LeftControl);
+        if (shoot)
+        {
+            shoot = false;
+            foreach(Gun gun in guns)
+            {
+                gun.Shoot();
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -33,6 +55,10 @@ public class Player : MonoBehaviour
         Vector2 pos = transform.position;
 
         float moveAmount = moveSpeed * Time.fixedDeltaTime;
+        if (speedUp)
+    {
+        moveAmount *= 3;
+    }
         Vector2 move = Vector2.zero;
 
     if (moveUp)
@@ -52,8 +78,43 @@ public class Player : MonoBehaviour
         move.x += moveAmount;
     }
 
+
+// Alla olevalla koodilla estetään liikkumisnopeuden kasvaminen jos liikutaan kahteen suuntaan yhtä aikaa:
+    float moveMagnitude = Mathf.Sqrt(move.x * move.x + move.y * move.y);
+    if (moveMagnitude > moveAmount)
+    {
+        float ratio = moveAmount / moveMagnitude;
+        move *= ratio;
+    }
+    moveMagnitude = Mathf.Sqrt(move.x * move.x + move.y * move.y);
+
     pos += move;
 
+
+
+
+
+
+
+
+//Rajoitetaan liikkuminen kameran alueelle:
+    if (pos.x <= 1.685599f )
+    {
+        pos.x = 1.685599f;
+    }
+    if (pos.x >= 15.6856f )
+    {
+        pos.x = 15.6856f;
+    }
+
+if (pos.y <= 1.159951f )
+    {
+        pos.y = 1.159951f;
+    }
+    if (pos.y >= 9.15995f )
+    {
+        pos.y = 9.15995f;
+    }
     transform.position = pos;
 
 
