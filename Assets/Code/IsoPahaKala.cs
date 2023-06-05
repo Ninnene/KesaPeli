5d17b970a15fe536dad1e3808661be5c91a69acb
Tonza81 <129681977+Tonza81@users.noreply.github.com>
2023-06-05 14:54:50 +0300

    Tein liikkumiskoodin pikkuvihollisille ja Isolle pahalle kalalle.

---
+++ Assets/Code/IsoPahaKala.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsoPahaKala : MonoBehaviour
+{
+
+    public float ipkSpeed = 5f;
+    public float ipkLeft = 100f;
+    public float ipkTimer = 1000f;
+
+    private void FixedUpdate()
+    {
+        ipkTimer -= 1f;
+
+        if (ipkTimer <=0)
+        {
+            Vector2 pos = transform.position;
+
+            pos.x -= ipkSpeed * Time.fixedDeltaTime;
+
+            ipkLeft -= 1f;
+
+             transform.position = pos;
+
+        }
+        if (ipkLeft <= 0)
+        {
+            IpkGoBack();
+        }
+    }
+
+    void IpkGoBack()
+    {
+
+        Vector2 pos = transform.position;
+
+        pos.x += ipkSpeed * Time.fixedDeltaTime;
+
+        ipkTimer += 10;
+
+        if (pos.x > 25.0568f )
+        {
+            ipkTimer = +100f;
+
+            ipkLeft = 10;
+
+            return;
+        }
+
+        transform.position = pos;
+
+    }
+
+
+       
+}
