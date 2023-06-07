94131dff5e61df9d1316f4573d7dd84177fa4e1a
Tonza81 <129681977+Tonza81@users.noreply.github.com>
2023-06-07 14:46:39 +0300

    Pikkuvihollisia voi nyt tuhota ampumalla mutta ne eivät tuhoudu elleivät ole lähellä kameraa.

---
+++ Assets/Code/Destructable.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    bool canBeDestoyed = false;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(transform.position.x < 20.15367)
+        {
+            canBeDestoyed = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+    
+        if(!canBeDestoyed)
+        {
+            return;
+        }
+
+        Debug.Log("Pam!");
+
+        Bullet bullet = collision.GetComponent<Bullet>();
+        if (bullet != null)
+        {
+            Destroy(gameObject);
+            Destroy(bullet.gameObject);
+        }
+    }
+
+
+}
