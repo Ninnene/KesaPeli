2cc2ca855eee487a64268f15a9278c4edec585a6
Ninnene <129729333+Ninnene@users.noreply.github.com>
2023-06-14 14:59:06 +0300

    Menuja
    -Korjasin rikki menneen sliderin
    - Tein slideristä sellaisen että se muuttaa volumea, mutta koska musiikkia ei ole se ei oikeasti tee mitään.
    - Aloitin Pausemenua Toistaiseksi se ei toimi

--- Assets/Code/Menu Scripts/MainmenuScript.cs
+++ Assets/Code/Menu Scripts/MainmenuScript.cs
@@ -7,11 +7,14 @@ public class MainmenuScript : MonoBehaviour
 {
     public void PlayGame ()
     {
+        //play nappi siirtyy build menussa seuraavana olevaan sceneen
+        //eli nykyinen scene + 1
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame ()
     {
+        //Debug.Log ("Quit");
         Application.Quit();
     }
 
