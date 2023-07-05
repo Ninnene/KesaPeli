8632e95dc410cf6bc9b330adbc6e7823edfcb4d5
Tonza81 <toni.viitala@vuoksi.fi>
2023-07-05 13:40:56 +0300

    Epilogissa healthbar piilossa ja Score tulee näkyviin kun Thanks for playing.

--- Assets/Code/HideHealthBar.cs
+++ Assets/Code/HideHealthBar.cs
@@ -11,6 +11,7 @@ public class HideHealthBar : MonoBehaviour
     Vector3 hiddenPlace = new Vector3(-10,20,0);
     
     public FloatingHealtbarP floatingHealthBar;
+    public float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,23 +21,58 @@ public class HideHealthBar : MonoBehaviour
 
         if (SceneManager.GetActiveScene().name == "Dialogue")
             {
-                floatingHealthBar.gameObject.SetActive(false);   
+                //floatingHealthBar.gameObject.SetActive(false);
+                //floatingHealthBar.transform.Translate(hiddenPlace);
+                
             }
 
                 if (SceneManager.GetActiveScene().name == "Epilogue")
                     {
                     floatingHealthBar.gameObject.SetActive(false);
+                    //floatingHealthBar.transform.Translate(hiddenPlace);
                     }
 
-                        else
+                        if (SceneManager.GetActiveScene().name == "Level1")
                         {
-                        floatingHealthBar.gameObject.SetActive(true);
+                        //floatingHealthBar.gameObject.SetActive(true);
+                        //floatingHealthBar.transform.Translate(startingPlace);
                         } 
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+    {   /*
+        floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
+
+        if (SceneManager.GetActiveScene().name == "Level1")
+                        {
+                        //floatingHealthBar.gameObject.SetActive(true);
+                        floatingHealthBar.transform.Translate(startingPlace);
+                        } 
+
+                        if (SceneManager.GetActiveScene().name == "Dialogue")
+                        {
+                            //floatingHealthBar.gameObject.SetActive(false);
+                            floatingHealthBar.transform.Translate(hiddenPlace);
+                        }*/
+
+                    /*
+                    floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
+
+
+                        if (SceneManager.GetActiveScene().name == "Dialogue")
+                        {
+                        float maxDistanceDelta = speed * Time.deltaTime;
+                        floatingHealthBar.transform.position = Vector3.MoveTowards(transform.position, hiddenPlace, maxDistanceDelta);
+                        }
+
+                            if (SceneManager.GetActiveScene().name == "Level1")
+                            {
+                            float maxDistanceDelta = speed * Time.deltaTime;
+                            floatingHealthBar.transform.position = Vector3.MoveTowards(transform.position, startingPlace, maxDistanceDelta);
+                            } 
+                            */
+                
+            }
+              
 }
