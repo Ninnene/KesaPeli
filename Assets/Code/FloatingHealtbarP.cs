608e156e22268fe533ac0335695474e7e69c9316
Tonza81 <toni.viitala@vuoksi.fi>
2023-07-05 10:59:40 +0300

    Healthbar katoaa teksikentissä, mutta jostain syystä ei palaudu jos pelin käybbistää uudelleen menusta....

--- Assets/Code/FloatingHealtbarP.cs
+++ Assets/Code/FloatingHealtbarP.cs
@@ -6,30 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class FloatingHealtbarP : MonoBehaviour
 {
+    // Piilotetaan hiparipalkki Dialogue- ja Epilogue sceneissä.
+    Vector3 startingPlace = new Vector3(0,0,0);
+
+    Vector3 hiddenPlace = new Vector3(-10,20,0);
+    // /
     [SerializeField] private Slider slider;
 
 
     void Start()
-    {
-            if (SceneManager.GetActiveScene().name == "Dialogue")
+    {   /*
+         if (SceneManager.GetActiveScene().name == "Dialogue")
             {
-                gameObject.transform.parent.gameObject.SetActive(false);
-                gameObject.SetActive(false);
-                Debug.Log("Disable player healtbar.");
-                
+                transform.Translate(hiddenPlace);
+                Debug.Log("I am at : " + transform.position);    
             }
+
                 if (SceneManager.GetActiveScene().name == "Epilogue")
                 {
-                    gameObject.transform.parent.gameObject.SetActive(false);
-                    gameObject.SetActive(false);
-                    Debug.Log("Disable player healtbar.");
+                    transform.Translate(hiddenPlace);
+                    Debug.Log("I am at : " + transform.position); 
                 }
 
-                    else
+                    else if (SceneManager.GetActiveScene().name == "Level1")
                     {
-                        gameObject.SetActive(true);
-                        gameObject.transform.parent.gameObject.SetActive(true);
-                    }
+                        transform.Translate(startingPlace);
+                        Debug.Log("I am at : " + transform.position); 
+                    }   */
     }
 
     public void UpdateHealthBar(float currentValue, float maxValue)
@@ -37,9 +40,44 @@ public class FloatingHealtbarP : MonoBehaviour
         slider.value = currentValue / maxValue;
     }
     // Update is called once per frame
+
+
+    void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "Level1")
+                        {
+                            gameObject.SetActive(true);
+                        } 
+    }
+
     void Update()
     {
-        
+       if (SceneManager.GetActiveScene().name == "Dialogue")
+            {
+                gameObject.SetActive(false);   
+            }
+
+                if (SceneManager.GetActiveScene().name == "Epilogue")
+                    {
+                    gameObject.SetActive(false);
+                    }
+
+
+                    
     }
 }
 
+
+                // Vanha tapa
+
+                /*gameObject.transform.parent.gameObject.SetActive(false);
+                gameObject.SetActive(false);
+                Debug.Log("Disable player healtbar.");
+
+                    gameObject.transform.parent.gameObject.SetActive(false);
+                    gameObject.SetActive(false);
+                    Debug.Log("Disable player healtbar.");
+
+                        gameObject.SetActive(true);*/
+
+                        // /
---
+++ Assets/Code/HideHealthBar.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HideHealthBar : MonoBehaviour
+{
+
+    Vector3 startingPlace = new Vector3(0,0,0);
+
+    Vector3 hiddenPlace = new Vector3(-10,20,0);
+    
+    public FloatingHealtbarP floatingHealthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
+
+
+        if (SceneManager.GetActiveScene().name == "Dialogue")
+            {
+                floatingHealthBar.gameObject.SetActive(false);   
+            }
+
+                if (SceneManager.GetActiveScene().name == "Epilogue")
+                    {
+                    floatingHealthBar.gameObject.SetActive(false);
+                    }
+
+                        else
+                        {
+                        floatingHealthBar.gameObject.SetActive(true);
+                        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
