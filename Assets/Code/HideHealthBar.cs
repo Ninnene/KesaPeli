9dcd6103e53c8c218302f6b3a6baa82d1538d0cf
Tonza81 <129681977+Tonza81@users.noreply.github.com>
2023-07-06 16:20:44 +0300

    Yritin löytää Deaktivoidun mittarin FindObjectOfTypeAll()-metodilla.

--- Assets/Code/HideHealthBar.cs
+++ Assets/Code/HideHealthBar.cs
@@ -16,27 +16,53 @@ public class HideHealthBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     { 
-        floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
-
 
         if (SceneManager.GetActiveScene().name == "Dialogue")
             {
-                //floatingHealthBar.gameObject.SetActive(false);
+                floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
+
+                floatingHealthBar.gameObject.SetActive(false);
                 //floatingHealthBar.transform.Translate(hiddenPlace);
                 
             }
 
                 if (SceneManager.GetActiveScene().name == "Epilogue")
                     {
+                    floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
+
                     floatingHealthBar.gameObject.SetActive(false);
                     //floatingHealthBar.transform.Translate(hiddenPlace);
                     }
 
-                        if (SceneManager.GetActiveScene().name == "Level1")
+                        /*if (SceneManager.GetActiveScene().name == "Level1")
                         {
                         //floatingHealthBar.gameObject.SetActive(true);
                         //floatingHealthBar.transform.Translate(startingPlace);
-                        } 
+                        } */
+
+                            if(SceneManager.GetActiveScene().name == "Level1")
+                            {
+                                Debug.Log("Scenemanager = Level1");
+
+                                FloatingHealtbarP[] allObjects = Resources.FindObjectsOfTypeAll<FloatingHealtbarP>();
+                                foreach (FloatingHealtbarP obj in allObjects)
+                                {
+                                    Debug.Log("FindObjectsOfTypeAll start");
+
+                                    if (obj.CompareTag("PHP SLD"))
+                                    {
+                                        Debug.Log("Tag found");
+                                        Debug.Log("obj.gameObject is = " + obj.gameObject);
+                                        Debug.Log("obj.gameObject is active before = " + obj.gameObject.activeSelf);
+                                        obj.gameObject.SetActive(true);
+                                        Debug.Log("obj.gameObject is active after = " + obj.gameObject.activeSelf);
+
+                                        
+                                        break;
+                                    }
+        }
+
+
     }
 
     // Update is called once per frame
@@ -74,5 +100,5 @@ public class HideHealthBar : MonoBehaviour
                             */
                 
             }
-              
+    }          
 }
