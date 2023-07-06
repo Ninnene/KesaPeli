61da475fd46e8ef13559d52216fe34e10975eedd
Tonza81 <129681977+Tonza81@users.noreply.github.com>
2023-07-06 13:51:11 +0300

    Nyt Score toimii kaikissa leveleissä.

--- Assets/Code/ScoreInDialogue.cs
+++ Assets/Code/ScoreInDialogue.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScoreInDialogue : MonoBehaviour
 {
@@ -9,26 +10,37 @@ public class ScoreInDialogue : MonoBehaviour
 
     public TMP_Text epilogueScoreNumberValue; 
 
-    public TMP_Text ScoreNumberLevel2; 
+    public TMP_Text scoreNumberLevel2; 
     int epilogueScore = 0;
+    int level2Score = 0;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        epilogueScoreNumber = GameObject.Find("---------------LevelController---------").GetComponent<LevelController>();
         
-        epilogueScore = epilogueScoreNumber.score;
-        
-        epilogueScoreNumberValue.text = epilogueScore.ToString();
 
-        ScoreNumberLevel2.text = epilogueScore.ToString();
+        Debug.Log("scoreNumberLevel2 = " + scoreNumberLevel2);
     }
 
     // Update is called once per frame
     void Update()
     {
+        epilogueScoreNumber = GameObject.Find("---------------LevelController---------").GetComponent<LevelController>();
         
+        epilogueScore = epilogueScoreNumber.score;
+        level2Score =  epilogueScoreNumber.score;
+        
+
+        if((SceneManager.GetActiveScene().name == "Epilogue"))
+        {
+        epilogueScoreNumberValue.text = epilogueScore.ToString();
+        }
+
+        if((SceneManager.GetActiveScene().name == "Level2"))
+        {
+        scoreNumberLevel2.text = level2Score.ToString();
+        }
     }
 }
