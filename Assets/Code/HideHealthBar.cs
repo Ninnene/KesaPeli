3afbb2120dd901ecc2ef11068a82c08f4940a556
Tonza81 <129681977+Tonza81@users.noreply.github.com>
2023-07-07 09:49:34 +0300

    Hiparimittarin palautus toimii.

--- Assets/Code/HideHealthBar.cs
+++ Assets/Code/HideHealthBar.cs
@@ -13,61 +13,74 @@ public class HideHealthBar : MonoBehaviour
     public FloatingHealtbarP floatingHealthBar;
     public float speed = 5f;
 
-    // Start is called before the first frame update
-    void Start()
-    { 
+        // Start is called before the first frame update
+        void Start()
+        { 
 
-        if (SceneManager.GetActiveScene().name == "Dialogue")
+            if (SceneManager.GetActiveScene().name == "Dialogue")
             {
-                floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
-
-                floatingHealthBar.gameObject.SetActive(false);
-                //floatingHealthBar.transform.Translate(hiddenPlace);
-                
-            }
-
-                if (SceneManager.GetActiveScene().name == "Epilogue")
-                    {
+                {
                     floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
 
                     floatingHealthBar.gameObject.SetActive(false);
                     //floatingHealthBar.transform.Translate(hiddenPlace);
-                    }
+                    
+                }
 
-                        /*if (SceneManager.GetActiveScene().name == "Level1")
+                    if (SceneManager.GetActiveScene().name == "Epilogue")
                         {
-                        //floatingHealthBar.gameObject.SetActive(true);
-                        //floatingHealthBar.transform.Translate(startingPlace);
-                        } */
+                        floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
 
-                            if(SceneManager.GetActiveScene().name == "Level1")
+                        floatingHealthBar.gameObject.SetActive(false);
+                        //floatingHealthBar.transform.Translate(hiddenPlace);
+                        }
+
+                            /*if (SceneManager.GetActiveScene().name == "Level1")
                             {
-                                Debug.Log("Scenemanager = Level1");
+                            //floatingHealthBar.gameObject.SetActive(true);
+                            //floatingHealthBar.transform.Translate(startingPlace);
+                            } */
 
-                                FloatingHealtbarP[] allObjects = Resources.FindObjectsOfTypeAll<FloatingHealtbarP>();
-                                foreach (FloatingHealtbarP obj in allObjects)
-                                {
-                                    Debug.Log("FindObjectsOfTypeAll start");
+            }
 
-                                    if (obj.CompareTag("PHP SLD"))
-                                    {
-                                        Debug.Log("Tag found");
-                                        Debug.Log("obj.gameObject is = " + obj.gameObject);
-                                        Debug.Log("obj.gameObject is active before = " + obj.gameObject.activeSelf);
-                                        obj.gameObject.SetActive(true);
-                                        Debug.Log("obj.gameObject is active after = " + obj.gameObject.activeSelf);
 
-                                        
-                                        break;
-                                    }
         }
 
-
-    }
-
     // Update is called once per frame
-    void Update()
-    {   /*
+            void Update()
+            {  
+                
+                
+                if(SceneManager.GetActiveScene().name == "Level1")
+                    {
+                        Debug.Log("Scenemanager = Level1");
+
+                        FloatingHealtbarP[] allObjects = Resources.FindObjectsOfTypeAll<FloatingHealtbarP>();
+                        foreach (FloatingHealtbarP obj in allObjects)
+                            {
+                                Debug.Log("FindObjectsOfTypeAll start");
+
+                                    if (obj.CompareTag("PHP SLD"))
+                                        {
+                                            Debug.Log("Tag found");
+                                            Debug.Log("obj.gameObject is = " + obj.gameObject);
+                                            Debug.Log("obj.gameObject is active before = " + obj.gameObject.activeSelf);
+                                            obj.gameObject.SetActive(true);
+                                            Debug.Log("obj.gameObject is active after = " + obj.gameObject.activeSelf);
+
+                                                
+                                            break;
+                                        }
+                                }
+                    }
+                          
+            }
+    }          
+
+
+
+
+ /*
         floatingHealthBar = GameObject.Find("PHP SLD").GetComponent<FloatingHealtbarP>();
 
         if (SceneManager.GetActiveScene().name == "Level1")
@@ -98,7 +111,3 @@ public class HideHealthBar : MonoBehaviour
                             floatingHealthBar.transform.position = Vector3.MoveTowards(transform.position, startingPlace, maxDistanceDelta);
                             } 
                             */
-                
-            }
-    }          
-}
